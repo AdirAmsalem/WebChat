882d0510e425ba1e5fb79f6a27aa6dfc0ee46f7d
Adir Amsalem <adiramsalem@hotmail.com>
2013-07-19 17:34:12 +0300

    Improved localization module

--- app/scripts/angular/filters/i18n.filter.js
+++ app/scripts/angular/filters/i18n.filter.js
@@ -1,29 +1,96 @@
-angular.module('Localization', []).factory('localization', ['$rootScope', '$http', '$q', function($rootScope, $http, $q) {
+angular.module('Localization', []).factory('localization', ['$rootScope', '$http', '$q', '$timeout', function($rootScope, $http, $q, $timeout) {
 
-	var cachedData,
-		supportedLanguages = ['en', 'he'],
-		language = navigator.language || navigator.userLanguage;
+	var LANG_DIRECTORY = 'scripts/lang',
+		langData,
+		language = (navigator.language || navigator.userLanguage).toLowerCase(),
+		supportedLanguages = [
+			{
+				name: 'English',
+				languages: ['en', 'en-us'],
+				file: 'en.json'
+			},
+			{
+				name: 'Hebrew',
+				languages: ['he', 'he-il'],
+				file: 'he.json'
+			}
+		];
+
+	function getLanguageList() {
+		var list = [];
+
+		// Basic memoization
+		if (this.list) {
+			return this.list;
+		}
+
+		for (var i = 0, lang; !!(lang = supportedLanguages[i]); i++) {
+			list.push({
+				name: lang.name,
+				value: lang.languages[0]
+			});
+		}
+
+		this.list = list;
+
+		return list;
+	}
 
 	function getLanguage() {
-		for (var key in supportedLanguages) {
-			if (supportedLanguages[key] === language) {
-				return language;
+		for (var i = 0, langObject; !!(langObject = supportedLanguages[i]); i++) {
+			for (var j = 0, lang; !!(lang = langObject.languages[j]); j++) {
+				if (lang === language) {
+					return langObject;
+				}
 			}
 		}
 
 		return supportedLanguages[0];
 	}
 
-	function getLocalization() {
+	function setLanguage(lang) {
+		if (lang && angular.isString(lang)) {
+			language = lang;
+		}
+	}
+
+	function getKey(key) {
+		if (langData && langData.keys && langData.keys[key]) {
+			return langData.keys[key];
+		}
+
+		return '';
+	}
+
+	function getData() {
 		var deferred = $q.defer();
 
-		function setLocalization(data) {
-			cachedData = data;
+		function getLangData() {
+			if (langData) {
+				deferred.resolve(langData);
+				return true;
+			}
+
+			$timeout(getLangData, 200);
+		}
+
+		getLangData();
+
+		return deferred.promise;
+	}
+
+	function getLocalization() {
+		var	deferred = $q.defer(),
+			seperator = LANG_DIRECTORY[LANG_DIRECTORY.length-1] !== '/' ? '/' : '',
+			file = getLanguage().file;
+
+		function setLangData(data) {
+			langData = data;
 			deferred.resolve(data);
 		}
 
-		$http.get('scripts/lang/' + getLanguage() + '.json')
-			.success(setLocalization)
+		$http.get(LANG_DIRECTORY + seperator + file, { cache: true })
+			.success(setLangData)
 			.error(deferred.reject);
 
 		return deferred.promise;
@@ -33,37 +100,19 @@ angular.module('Localization', []).factory('localization', ['$rootScope', '$http
 		console.error('Can\'t load localization file.');
 	});
 
+	// Publish API
 	return {
-		getKey: function(key) {
-			if (cachedData && cachedData.keys && cachedData.keys[key]) {
-				return cachedData.keys[key];
-			}
-			return '';
-		},
-		getData: function() {
-			var deferred = $q.defer();
-
-			function getCache() {
-				if (cachedData) {
-					deferred.resolve(cachedData);
-					return true;
-				}
-
-				setInterval(getCache, 200);
-			}
-
-			getCache();
-
-			return deferred.promise;
-		}
+		getLanguageList: getLanguageList,
+		getKey: getKey,
+		getData: getData,
 	};
 
 }])
 .filter('i18n', ['localization', function(localization) {
 	return function(key) {
-		if (!key || !angular.isString(key)) {
-			return '';
+		if (key && angular.isString(key)) {
+			return localization.getKey(key);
 		}
-		return localization.getKey(key);
+		return '';
 	};
 }]);
